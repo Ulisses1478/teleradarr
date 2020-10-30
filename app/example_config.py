d3ef58df200221167127c25ca6bdd40b13f55abf
Ulisses1478 <silvamunizulisses@hotmail.com>
2020-10-30 18:30:52 -0300

    Example config file

---
+++ app/example_config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'radarr':{
+        'baseUrl':'http://ip:port/radarr', # IF USING SUFFIX PUT HERE
+        'apiKey':'7ay7dfas7df79ab70742709471907asdf' # Can be found in credentials
+    },
+    'telegram':{
+        'botToken':'number:something' # Get with telegram BotFather
+    }
+}
